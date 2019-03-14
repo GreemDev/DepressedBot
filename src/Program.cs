682970de58b09fd8dd9f890225eb574e522c972d
Michael Husted <gr33m11@gmail.com>
2019-03-13 19:23:54 -0500

    init

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DepressedBot
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
