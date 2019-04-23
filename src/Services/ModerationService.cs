fd8eeec9be942d155a74af851c975a78c01d3e5c
Michael Husted <gr33m11@gmail.com>
2019-04-22 23:31:29 -0500

    Music channel whitelist.

--- src/Services/ModerationService.cs
+++ src/Services/ModerationService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using DepressedBot.Data.Objects.EventArgs;
+using Discord;
+using Gommon;
 
 namespace DepressedBot.Services
 {
@@ -8,15 +10,29 @@ namespace DepressedBot.Services
     {
         public async Task OnMessageReceivedAsync(MessageReceivedEventArgs args)
         {
-            await Rules(args);
+            await CheckRulesChannelAsync(args);
+            await CheckMusicChannelsAsync(args);
         }
 
-        private async Task Rules(MessageReceivedEventArgs args)
+        private async Task CheckRulesChannelAsync(MessageReceivedEventArgs args)
         {
             if (args.Message.Channel.Id != 452962712318902303) return;
             if (args.Message.Author.Id != 168548441939509248 && args.Message.Author.Id != 385899728283500546) //hardcoded ids reeee
             {
                 await args.Message.DeleteAsync();
+            }
+        }
+
+        public async Task CheckMusicChannelsAsync(MessageReceivedEventArgs args)
+        {
+            if (args.Context.Channel.CategoryId.GetValueOrDefault() != 542855804727066636) return;
+            if (args.Context.Channel.Topic.ContainsIgnoreCase("whitelist-off")) return;
+            if (!args.Context.Channel.Topic.Contains($"{args.Context.User.Id}"))
+            {
+                await args.Message.DeleteAsync(new RequestOptions
+                {
+                    AuditLogReason = "Automatically deleted because the user in question's ID was not in the channel topic."
+                });
             }
         }
     }
